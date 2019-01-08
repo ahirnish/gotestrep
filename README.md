9a68a364aa75459c52a4ed60aefbdd909e192c6c
ahirnish <ahirnish@gmail.com>
2019-01-08 11:46:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# gotestrep
---
+++ ellipse.go
@@ -0,0 +1,14 @@
+package test_my_package
+
+import (
+ "math"
+)
+
+type Init struct {
+     A,B float64
+}
+
+func (e *Init) GetEccentricity() float64 {
+     return (math.Sqrt(math.Pow(e.A, 2) - math.Pow(e.B, 2))) / e.A
+}
+
